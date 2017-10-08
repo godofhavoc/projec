9b4638ff2234f67341e159d1fb9ab40006e75436
shoaibbhimani <shoaibbhimani1392@gmail.com>
2017-10-08 23:10:44 +0530

    slider-button-clicked-activated

---
+++ tours/custom.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  $(".month").slick({
+    slidesToShow: 5,
+    infinite: true,
+    slidesToScroll: 3,
+    speed: 500,
+    centerMode: true
+  });
+
+  $(".tours-list").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000
+  });
+
+  var $galleryContainer = $(".gallery-container");
+
+  $galleryContainer.slick();
+
+  $galleryContainer.find(".gallery-left-arrow").on("click", function() {
+    $galleryContainer.slick("slickPrev");
+  });
+
+  $galleryContainer.find(".gallery-right-arrow").on("click", function() {
+    $galleryContainer.slick("slickNext");
+  });
+});
